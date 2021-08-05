644bcad2111000407d89832d7012a445ee2f1e42
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-08-05 13:45:49 -0400

    Construyendo banner

--- src/app.py
+++ src/app.py
@@ -12,5 +12,22 @@ df = pd.read_csv('../assets/Covid19VacunasAgrupadas.csv')
 
 #print(df.vacuna_nombre.nunique())
 
-print(df.vacuna_nombre.unique())
+#print(df.vacuna_nombre.unique())
 
+#inicializando aplicacion de python por el parametro name
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+
+    html.Div([
+
+        html.H1('Vacunados por covid'),
+        html.Img(src='../assets/COVID-19-outbreak-timeline.gif')
+    ], className = 'banner')
+])
+
+if __name__ == '__main__':
+    app.run_server(
+        debug=True #port=8080
+        )
